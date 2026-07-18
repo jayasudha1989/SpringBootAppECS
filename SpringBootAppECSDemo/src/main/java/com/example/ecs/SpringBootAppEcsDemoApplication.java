181027189e5ed1721be638e027ca733feb976020
jayasudha1989 <100898392+jayasudha1989@users.noreply.github.com>
2026-07-18 21:56:09 +0800

    Add files via upload
    Initial commit

---
+++ SpringBootAppECSDemo/src/main/java/com/example/ecs/SpringBootAppEcsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppEcsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppEcsDemoApplication.class, args);
+	}
+
+}
